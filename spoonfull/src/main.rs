17a84b6b86fca73a3cb51de5c6966e9724aa373f
Pierre-Emmanuel Patry <pierre-emmanuel.patry@epita.fr>
2022-06-03 12:57:32 +0200

    feat(mqtt): Add mqtt subscriptions

--- spoonfull/src/main.rs
+++ spoonfull/src/main.rs
@@ -57,10 +57,34 @@ fn rocket() -> Rocket<Build> {
 
         let config: Config = confy::load_path(PathBuf::from("config/rumqttd.conf")).unwrap();
         info_!("{}: {}", "config", Paint::default("loading configuration"));
-        let output = Broker::new(config).start();
+        let mut broker = Broker::new(config);
+        let mut tx = broker.link("localclient").unwrap();
+        let output = broker.start();
         info!("{}{}:", Paint::emoji("🛑"), Paint::yellow(" MQTT"));
         info_!("{}: {}", "broker stopped", Paint::default(format!("{:?}", output)));
-    });
 
-    build
+        let mut rx = tx.connect(200).unwrap();
+        tx.subscribe("#").unwrap();
+
+        // subscribe and publish in a separate thread
+        thread::spawn(move || {
+            for _ in 0..10 {
+            for i in 0..200 {
+                let topic = format!("hello/{}/world", i);
+                tx.publish(topic, false, vec![0; 1024]).unwrap();
+            }
+            }
+        });
+
+        let mut count = 0;
+        loop {
+            if let Some(message) = rx.recv().unwrap() {
+            // println!("T = {}, P = {:?}", message.topic, message.payload.len());
+            count += message.payload.len();
+            println!("{}", count);
+            }
+        }
+        });
+
+        build
 }
